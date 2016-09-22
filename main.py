fdbbea5b427df9b72f89cd45c0f568a4c644ccf9
Henry Sissac <thesissach@gmail.com>
2016-09-21 21:16:51 -0500

    Final Work of Build a Blog

--- main.py
+++ main.py
@@ -14,12 +14,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        h = jinja_env.get_template(template)
+        return h.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Opinion(db.Model):
+    title = db.StringProperty(required = True)
+    elaboration = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Signup(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write("Type /blog into the url!")
 
+class ViewPostHandler(Handler):
+    def render_post(self, title="", elaboration="", error=""):
+        self.render("front.html", title=title, elaboration=elaboration, error=error)
+    def get(self, id):
+        t = Opinion.get_by_id(int(id))
+        post_title=t.title
+        post_elaboration= t.elaboration
+        self.render("singlepost.html", title=post_title, elaboration=post_elaboration)
+
+
+
+class MainPage(Handler):
+    def render_new(self, title="", elaboration="", error=""):
+        opinions = db.GqlQuery("SELECT * FROM Opinion "
+                            "ORDER BY created DESC "
+                            "LIMIT 5")
+        self.render("front.html", title=title, elaboration=elaboration, error=error, opinions=opinions)
+
+    def get(self):
+        self.render_new()
+
+class NewEntry(Handler):
+    def render_post(self, title="", elaboration="", error=""):
+        self.render("newpost.html", title=title, elaboration=elaboration, error=error)
+    def get(self):
+        self.render_post()
+    def post(self):
+        title = self.request.get("title")
+        elaboration = self.request.get("elaboration")
+
+        if title and elaboration:
+            e = Opinion(title = title, elaboration = elaboration)
+            e.put()
+
+            self.redirect("/blog")
+        else:
+            error = "To submit a post, you need to include both a title and body."
+            self.render_post(title=title, elaboration=elaboration, error=error)
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/blog', MainPage),('/blog/newpost', NewEntry),('/',Signup), webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
